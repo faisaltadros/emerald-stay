5e12c31072aa87c166e034dfb2175ad6832dc93f
Faisal <faisal@rotatestudios.com>
2019-10-24 23:45:24 +0200

    Updates

---
+++ precache-manifest.81a5d0f9f4ffb80f9211692acde694cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "492b1a65654726050494b0acc0e6af2f",
+    "url": "/emerald-stay/index.html"
+  },
+  {
+    "revision": "513d225d4ff8b3553463",
+    "url": "/emerald-stay/static/css/main.e123d1a8.chunk.css"
+  },
+  {
+    "revision": "70e9b138bc228ff84bd3",
+    "url": "/emerald-stay/static/js/2.baa78415.chunk.js"
+  },
+  {
+    "revision": "513d225d4ff8b3553463",
+    "url": "/emerald-stay/static/js/main.b7c1b1fc.chunk.js"
+  },
+  {
+    "revision": "e918b0144b8fccc90be2",
+    "url": "/emerald-stay/static/js/runtime-main.53ba54b3.js"
+  },
+  {
+    "revision": "f2e6e8327e788473741dcfb399fc44a1",
+    "url": "/emerald-stay/static/media/logo.f2e6e832.svg"
+  }
+]);
